36a453536c24c659b4dfc4651a8781623177e10d
0x97 <releed@outlook.com>
2021-08-04 17:53:20 +0800

    wqqwe
    qweee

---
+++ plugins/lut/edittime.js
@@ -0,0 +1,228 @@
+﻿/*
+2D Starfield -- code inspired from web and C2 plugins Authors 
+-V 1.3 fixed auto update when nothing moves newt from C2 
+-V 1.2 Hotspot for stunning rotation 
+-V 1.1 Added Star Base color wich is shaded to darker ... so maybe layer 5 is not visible ! 
+-V 1.0 initial internal release for test 
+*/
+
+function GetPluginSettings()  
+{
+	return {
+		"name":			"Look Up Table",
+		"id":			"c2lut",
+		"version":		"1.0",					// (float in x.y format) Behavior version - C2 shows compatibility warnings based on this
+		"description":	"Look Up Color Table",
+		"author":		"Gigatron",
+		"help url":		"http://www.scirra.com",
+		"category":		"Auto Run Plugin",
+		"type":			"world",			// appears in layout
+		"rotatable":	true,
+		"flags":		pf_position_aces | pf_size_aces | pf_angle_aces | pf_appearance_aces | pf_zorder_aces 
+		
+	};
+};
+
+// Conditions, actions and expressions
+AddComboParamOption("(none)");
+AddComboParamOption("Additive");
+AddComboParamOption("XOR");
+AddComboParamOption("Copy");
+AddComboParamOption("Destination over");
+AddComboParamOption("Source in");
+AddComboParamOption("Destination in");
+AddComboParamOption("Source out");
+AddComboParamOption("Destination out");
+AddComboParamOption("Source atop");
+AddComboParamOption("Destination atop");
+AddComboParam("Effect", "Choose the new effect for this object.");
+AddAction(1, 0, "Set effect", "Appearance", "Set effect to <i>{0}</i>", "Set the rendering effect of this object.", "SetEffect");
+
+AddObjectParam("Sprite", "Choose Base Texture Sprite");
+AddAction(8, 0, "Set Base Texture", "Glut", "Base Texture to {0}", "Set Base Texture", "SetTexture");
+
+AddObjectParam("Sprite", "Choose Color Look Up Table Texture");
+AddAction(9, 0, "Set Lut Texture", "Glut ", "Lut Texture to {0}", "Set Lut Texture", "SetGrad");
+
+
+
+AddNumberParam("X Position", "Set Screen X Position");
+AddAction(20, 0, "Set Screen X Position ", "Glut", "Set Screen X Position to {0}", "Set Screen X Position x =1/Width of Sprite Area", "SetXpos");
+
+
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	
+	new cr.Property(ept_combo,	"Effect",				"(none)",	"Choose an effect for this object.  (This does not preview in the layout, only when you run.)", "(none)|Additive|XOR|Copy|Destination over|Source in|Destination in|Source out|Destination out|Source atop|Destination atop"),
+	 
+	new cr.Property(ept_color,	"Color 0",	cr.RGB(255,255, 255),	"For Future Usage ! "),
+	
+	new cr.Property(ept_combo,	"Initial visibility",	"Visible",	"Choose whether the object is visible when the layout starts.", "Visible|Invisible"),
+	new cr.Property(ept_combo,	"Hotspot",				"Top-left",	"Choose the location of the hot spot in the object.", "Top-left|Top|Top-right|Left|Center|Right|Bottom-left|Bottom|Bottom-right"),    
+	
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// Plugin-specific variables
+	this.just_inserted = false;
+}
+
+IDEInstance.prototype.OnCreate = function()
+{
+
+	switch (this.properties["Hotspot"])
+	{
+    case "Top-left" :
+      this.instance.SetHotspot(new cr.vector2(0, 0));
+      break;
+    case "Top" :
+      this.instance.SetHotspot(new cr.vector2(0.5, 0));
+      break;
+    case "Top-right" :
+      this.instance.SetHotspot(new cr.vector2(1, 0));
+      break;
+    case "Left" :
+      this.instance.SetHotspot(new cr.vector2(0, 0.5));
+      break;
+    case "Center" :
+      this.instance.SetHotspot(new cr.vector2(0.5, 0.5));
+      break;
+    case "Right" :
+      this.instance.SetHotspot(new cr.vector2(1, 0.5));
+      break;
+    case "Bottom-left" :
+      this.instance.SetHotspot(new cr.vector2(0, 1));
+      break;
+    case "Bottom" :
+      this.instance.SetHotspot(new cr.vector2(0.5, 1));
+      break;
+    case "Bottom-right" :
+		  this.instance.SetHotspot(new cr.vector2(1, 1));
+      break;
+	}
+}
+
+IDEInstance.prototype.OnInserted = function()
+{
+	this.just_inserted = true;
+}
+
+IDEInstance.prototype.OnDoubleClicked = function()
+{
+	 
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	// Edit image link
+	 
+	
+	if (property_name === "Image")
+	{
+		this.instance.EditTexture();
+	}
+	else if (property_name === "Hotspot")
+	{
+    switch (this.properties["Hotspot"])
+    {
+      case "Top-left" :
+        this.instance.SetHotspot(new cr.vector2(0, 0));
+      break;
+      case "Top" :
+        this.instance.SetHotspot(new cr.vector2(0.5, 0));
+      break;
+      case "Top-right" :
+        this.instance.SetHotspot(new cr.vector2(1, 0));
+      break;
+      case "Left" :
+        this.instance.SetHotspot(new cr.vector2(0, 0.5));
+      break;
+      case "Center" :
+        this.instance.SetHotspot(new cr.vector2(0.5, 0.5));
+      break;
+      case "Right" :
+        this.instance.SetHotspot(new cr.vector2(1, 0.5));
+      break;
+      case "Bottom-left" :
+        this.instance.SetHotspot(new cr.vector2(0, 1));
+      break;
+      case "Bottom" :
+        this.instance.SetHotspot(new cr.vector2(0.5, 1));
+      break;
+      case "Bottom-right" :
+        this.instance.SetHotspot(new cr.vector2(1, 1));
+      break;
+    }
+	}
+}
+
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+	 
+}
+	
+// Called to draw self in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+	//var isCheckbox = (this.properties["Type"] === "Checkbox");
+	
+	if (!this.font)
+		this.font = renderer.CreateFont("Arial", 24, false, false);
+		
+	renderer.SetTexture(null);
+	var quad = this.instance.GetBoundingQuad();
+
+	renderer.Fill(quad, cr.RGB(20, 150, 150));
+	renderer.Outline(quad, cr.RGB(0, 0, 0));
+		
+	cr.quad.prototype.offset.call(quad, 0, 2);
+
+	this.font.DrawText(this.properties["Text"],
+							quad,
+							cr.RGB(this.properties["Color"]),
+							ha_center);
+	
+	 
+}
+
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+	 
+}
